58d1d36361c7285f697518c8124c5a5af7fe7b97
tatyananegodyaeva <tashanegodyaeva@gmail.com>
2026-06-18 12:10:50 +0300

    resolving task 104. Maximum Depth of Binary Tree

---
+++ src/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,23 @@
+public class MaximumDepthOfBinaryTree {
+    public int maxDept = 0;
+    static void main() {
+
+        TreeNode node1 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(15);
+        TreeNode node6 = new TreeNode(7);
+
+        TreeNode node2 = new TreeNode(20, node3, node6);
+        TreeNode root = new TreeNode(3, node1, node2);
+
+        System.out.println(maxDepth(root));
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftDept = maxDepth(root.left);
+        int rightDept = maxDepth(root.right);
+
+        return Math.max(leftDept, rightDept) + 1;
+    }
+}
