46bc1f4b44e4957ba0ab4615c2c45653459070d9
tatyananegodyaeva <tashanegodyaeva@gmail.com>
2026-07-09 12:34:13 +0300

    resolving task 121. Best Time to Buy and Sell Stock with 2 ways (SW and Greedy)

--- src/BestTimeBuySellStock.java
+++ src/BestTimeBuySellStock.java
@@ -2,28 +2,41 @@ public class BestTimeBuySellStock {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.maxProfit(new int[] {7,6,4,3,1}));
+        System.out.println(solution.maxProfit(new int[]{7, 6, 4, 3, 1}));
 
     }
 
     static class Solution {
         public int maxProfit(int[] prices) {
-            int buy = prices[0];
-            int sell;
-            int profit = 0;
+            int minPrice = prices[0];
+            int maxProfit = 0;
 
             for (int price : prices) {
-                if (price < buy) {
-                    buy = price;
+                minPrice = Math.min(minPrice, price);
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+
+            return maxProfit;
+
+        }
+
+        public int maxProfitWithWindow(int[] prices) {
+            int l = 0;
+            int r = l + 1;
+            int profit = 0;
+
+            while (r < prices.length) {
+                if (prices[r] < prices[l]) {
+                    l = r;
+                } else {
+                    profit = Math.max(profit, prices[r] - prices[l]);
                 }
 
-                sell = price;
-
-                profit = Math.max(profit, sell - buy);
-
+                r++;
             }
 
             return profit;
+
         }
     }
 
