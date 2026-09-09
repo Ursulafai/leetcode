3ccfa54d519039da20145d1ca5bf627180db6ee3
tatyananegodyaeva <tashanegodyaeva@gmail.com>
2026-09-09 19:42:35 +0300

    resolving task 155. Min Stack

---
+++ src/MinStack.java
@@ -0,0 +1,41 @@
+public class MinStack {
+    private Node head;
+
+    public MinStack() {
+        head = null;
+    }
+
+    public void push(int value) {
+        if (head == null) {
+            head = new Node(value, value, null);
+        } else {
+            head = new Node(value, Math.min(value, head.min), head);
+        }
+
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        public Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+}
